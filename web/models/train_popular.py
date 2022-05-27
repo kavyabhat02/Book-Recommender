6ce2735402308b087b05d93b58c1758009c634dd
kavyabhat02 <kavyabhat@gmail.com>
2022-05-27 11:18:42 +0530

    fix path, add headers in csv

--- web/models/train_popular.py
+++ web/models/train_popular.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-import pandas as pd
 import pickle
+import numpy as np 
+import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-df = pd.read_csv('../books.csv',error_bad_lines = False)
+df = pd.read_csv('./web/books.csv')
 df.head()
 
 df.fillna(0,inplace=True)
@@ -18,9 +20,6 @@ df2.loc[ (df2['average_rating'] > 2) & (df2['average_rating'] <= 3), 'rating_bet
 df2.loc[ (df2['average_rating'] > 3) & (df2['average_rating'] <= 4), 'rating_between'] = "between 3 and 4"
 df2.loc[ (df2['average_rating'] > 4) & (df2['average_rating'] <= 5), 'rating_between'] = "between 4 and 5"
 
-#df.replace([np.inf, -np.inf], np.nan, inplace=True)
-#df.fillna(999, inplace=True)
-
 rating_df = pd.get_dummies(df2['rating_between'])
 
 features = pd.concat([rating_df, 
@@ -31,10 +30,9 @@ from sklearn.preprocessing import MinMaxScaler
 min_max_scaler = MinMaxScaler()
 features = min_max_scaler.fit_transform(features)
 
-model = neighbors.NearestNeighbors(n_neighbors=10, algorithm='ball_tree')
+model = neighbors.NearestNeighbors(n_neighbors=6, algorithm='ball_tree')
 model.fit(features)
 dist, idlist = model.kneighbors(features)
 
 pickledModel = pickle.dumps(df2)
 pickledList = pickle.dumps(idlist)
-
