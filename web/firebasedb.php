ca486476350dedb33f1536ddd6bcd5d74a044930
kavyabhat02 <kavyabhat@gmail.com>
2022-05-28 18:12:22 +0530

    test

--- web/firebasedb.php
+++ web/firebasedb.php
@@ -2,7 +2,7 @@
   require __DIR__."/../vendor/autoload.php";
 
   use Kreait\Firebase\Factory;
-  use Kreait\Firebase\Contract\Auth;
+  use Kreait\Firebase\Auth;
   $factory = (new Factory)->withServiceAccount(__DIR__.'/bookbuddy-d0ee9-firebase-adminsdk-t1x7z-cae16e7b9a.json');
   $auth = $factory->createAuth();
 ?>
