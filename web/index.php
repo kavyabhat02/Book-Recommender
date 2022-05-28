50f141ec7b7cd5a90606ef087faea78e7b66c348
kavyabhat02 <kavyabhat@gmail.com>
2022-05-28 21:37:06 +0530

    fix extension

--- web/index.php
+++ web/index.php
@@ -51,7 +51,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <a class="nav-link pl-lg-0" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link pl-lg-0" href="index.php">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="./html/categories.html">Categories</a>
