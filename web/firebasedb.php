d533ef2fda0dd19887990a9f9a0eb239dcab98c2
kavyabhat02 <kavyabhat@gmail.com>
2022-05-28 17:29:26 +0530

    fixed path

--- web/firebasedb.php
+++ web/firebasedb.php
@@ -3,10 +3,7 @@
 
   use Kreait\Firebase\Factory;
   use Kreait\Firebase\Contract\Auth;
-  $factory = (new Factory)
-    ->withServiceAccount('bookbuddy-d0ee9-firebase-adminsdk-t1x7z-cae16e7b9a.json')
-    ->withDatabaseUri('https://bookbuddy-d0ee9-default-rtdb.firebaseio.com/');
-  
+  $factory = (new Factory)->withServiceAccount(__DIR__.'/bookbuddy-d0ee9-firebase-adminsdk-t1x7z-cae16e7b9a.json');
   $database = $factory->createDatabase();
 
   $auth = $factory->createAuth();
